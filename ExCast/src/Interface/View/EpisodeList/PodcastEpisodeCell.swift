9fb1b92ae94df8deee952b5a81b7a7634f407371
tasuwo <kamuhata.you@gmail.com>
2019-08-03 19:08:54 +0900

    :construction: Prepare playing mark.

--- ExCast/src/Interface/View/EpisodeList/PodcastEpisodeCell.swift
+++ ExCast/src/Interface/View/EpisodeList/PodcastEpisodeCell.swift
@@ -23,6 +23,7 @@ class PodcastEpisodeCell: UITableViewCell {
     @IBOutlet weak var episodeDescription: UILabel!
     @IBOutlet weak var length: UILabel!
     @IBOutlet weak var informationButton: UIButton!
+    @IBOutlet weak var playingMarkIconView: UIImageView!
 
     var episode: Podcast.Episode?
     weak var delegate: PodcastEpisodeCellDelegate?
@@ -55,5 +56,18 @@ extension PodcastEpisodeCell: PodcastEpisodeCellProtocol {
         }
 
         self.informationButton.setTitle("Detail", for: .normal)
+        self.playingMarkIconView.image = generatePlayingMark()
+        self.playingMarkIconView.isHidden = true
+    }
+
+    private func generatePlayingMark() -> UIImage {
+        let renderer = UIGraphicsImageRenderer(size: CGSize(width: 10, height: 10))
+        let image = renderer.image { ctx in
+            let circle = UIBezierPath(arcCenter: CGPoint(x: 5, y: 5), radius: 2.5, startAngle: 0, endAngle: CGFloat(Double.pi)*2, clockwise: true)
+            ctx.cgContext.setFillColor(UIColor.red.cgColor)
+            ctx.cgContext.addPath(circle.cgPath)
+            ctx.cgContext.drawPath(using: .fill)
+        }
+        return image
     }
 }
