d6cfd7ae2a02392b1206303cca06540b7ff22f26
tasuwo <kamuhata.you@gmail.com>
2019-08-13 01:02:59 +0900

    :construction: Add custom notification interface.

---
+++ ThumbnailNotification/NotificationViewController.swift
@@ -0,0 +1,26 @@
+//
+//  NotificationViewController.swift
+//  ThumbnailNotification
+//
+//  Created by Tasuku Tozawa on 2019/08/13.
+//  Copyright © 2019 Tasuku Tozawa. All rights reserved.
+//
+
+import UIKit
+import UserNotifications
+import UserNotificationsUI
+
+class NotificationViewController: UIViewController, UNNotificationContentExtension {
+
+    @IBOutlet var label: UILabel?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any required interface initialization here.
+    }
+    
+    func didReceive(_ notification: UNNotification) {
+        self.label?.text = notification.request.content.body
+    }
+
+}
